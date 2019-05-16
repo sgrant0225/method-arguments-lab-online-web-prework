5f3bc2a8f1cd136bba88c79efa57e435a36ba887
Sharnell Grant <sharnellgrant@gmail.com>
2019-05-16 13:13:44 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+
+def introduction_with_lanuage(name, language)
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
+    introduction_with_language("Dan", "Ember.js")
+  end
